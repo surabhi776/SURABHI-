1a30a3e6726de86ed121211d213aaa8aa16da60c
surabhi776 <141718109+surabhi776@users.noreply.github.com>
2025-03-12 21:40:34 +0530

    Task 39
    task 39: 
    Create a small project in spring boot and create a api that return a String “welcome to Spring boot” using GET and test on POSTMAN

---
+++ UseRoneApplication.java
@@ -0,0 +1,13 @@
+package UserfFrstProject.USERone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseRoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseRoneApplication.class, args);
+	}
+
+}
